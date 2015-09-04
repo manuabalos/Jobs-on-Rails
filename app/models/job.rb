9d46418397954a0fdc9444d00340f7e9cb677ef4
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-09-04 13:11:40 +0200

    Fixed routes, añadidos metodos scraping para TrabajosRails y metodo Save

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,50 @@
 class Job < ActiveRecord::Base
+
+	def self.scrapingTrabajosRails
+		pageTrabajoRails = Mechanize.new
+		pageTrabajoRails = pageTrabajoRails.get("http://www.trabajosrails.com/")
+		
+		pageTrabajoRails.search(".jobs .job-entry").each do |jobpreview|
+			@URL = jobpreview.at(".job-title a")['href']
+
+			pageTrabajoRailsJob = Mechanize.new
+			pageTrabajoRailsJob = pageTrabajoRailsJob.get("http://www.trabajosrails.com"+@URL)
+
+			title = pageTrabajoRailsJob.at("header h1").text
+
+			date = pageTrabajoRailsJob.at("header .date").text
+
+			salary = "Undefined"
+
+			contract_type = "Undefined"
+
+			description = ""
+			pageTrabajoRailsJob.search(".job_description p").each do |paragraph|
+				description << paragraph.text
+			end
+
+			company = pageTrabajoRailsJob.at(".company a").text
+
+			country = pageTrabajoRailsJob.at(".company span").text
+
+			contact = pageTrabajoRailsJob.at(".contact-info-container p a").text
+
+			self.saveInfo(title, date, salary, contract_type, description, company, country, contact)
+		end
+	end
+
+	def self.saveInfo(title, date, salary, contract_type, description, company, country, contact)
+		job = Job.new
+
+		job.title = title
+		job.date = date
+		job.salary = salary
+		job.contract_type = contract_type
+		job.description = description
+		job.company = company
+		job.country = country
+		job.contact_URL = contact
+
+		job.save
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   root "home#index"
+
   resources :jobs
-  
+  get '/scraping', to: 'jobs#scraping', as: 'scraping'
+
 end
